9d9cced9ebf3581e7a7dbc6c80ff9118910257a2
Artem Gorbach <gorbacartem90@gmail.com>
2020-07-02 11:08:25 +0300

    add toggle functioanality in video components

--- app/screens/components/VideoView.tsx
+++ app/screens/components/VideoView.tsx
@@ -1,28 +1,65 @@
 // react
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, TouchableWithoutFeedback} from 'react-native';
+// components
+import DarkBgContainer from '../../components/DarkBgContainer';
+// icons
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 // video
 import Video from 'react-native-video';
 // assets
 import Styles from 'assets/styles/styles';
+// colors
+import {Colors} from 'assets/styles/constants';
 
 const VideoView: React.FC<{media_url: string}> = ({media_url}) => {
+  const [isSound, setIsSound] = useState(false);
+
+  const toggleSound = () => {
+    setIsSound((prev) => !prev);
+  };
+
+  const volume = isSound ? 1 : 0;
+  const soundIconName = `volume-${isSound ? 'high' : 'off'}`;
+
   return (
-    <View>
-      <Video
-        source={{uri: media_url}}
-        style={[Styles.fullScreen, styles.container]}
-        paused={false}
-        repeat={true}
-        resizeMode="cover"
-        volume={0}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={toggleSound}>
+      <View style={styles.container}>
+        <DarkBgContainer style={styles.iconContainer}>
+          <MaterialCommunityIcons
+            name={soundIconName}
+            style={styles.icon}
+            size={18}
+          />
+        </DarkBgContainer>
+        <Video
+          source={{uri: media_url}}
+          style={[Styles.fullScreen, styles.mediaContainer]}
+          paused={false}
+          repeat={true}
+          resizeMode="cover"
+          volume={volume}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default VideoView;
 
 const styles = StyleSheet.create({
-  container: {height: '100%'},
+  container: {position: 'relative'},
+  mediaContainer: {height: '100%'},
+  iconContainer: {
+    position: 'absolute',
+    bottom: 10,
+    right: 0,
+    zIndex: 999,
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    borderRadius: 100,
+  },
+  icon: {
+    color: Colors.WHITE,
+  },
 });

6ff3f5625b64a30de48675d72238c1810df1b80e
Artem Gorbach <gorbacartem90@gmail.com>
2020-07-02 17:45:02 +0300

    add rest time to video viewsa

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -6,3 +6,14 @@ export const formatToHumanReadable = (timestamp: string): string => {
   const utcDate = parse(timestamp, "yyyy-MM-dd'T'HH:mm:ssxxxx", new Date());
   return formatDistanceToNow(utcDate);
 };
+
+const addNull = (time: number) => {
+  const zero = +time === 0 || time < 10 ? '0' : '';
+  return zero + time;
+};
+
+export const formatToTimer = (sec: number): string => {
+  const seconds = Math.floor(sec % 60);
+  const minutes = Math.floor((sec / 60) % 60);
+  return `${addNull(minutes)}:${addNull(seconds)}`;
+};

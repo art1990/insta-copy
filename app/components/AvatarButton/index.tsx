17bd5eb303a0e932559d898bf6b084f90d0ff155
Artem Gorbach <gorbacartem90@gmail.com>
2020-07-06 22:34:49 +0300

    add profile loader

--- app/components/AvatarButton/index.tsx
+++ app/components/AvatarButton/index.tsx
@@ -1,5 +1,5 @@
 // react
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {
   StyleSheet,
   View,
@@ -7,6 +7,7 @@ import {
   GestureResponderEvent,
   Alert,
   Text,
+  Animated,
 } from 'react-native';
 // components
 import Avatar, {IAvatarProps} from 'components/Avatar';
@@ -29,8 +30,34 @@ const AvatarButton: React.FC<IAvatarButtonProps> = ({
   style,
   ...rest
 }) => {
+  const animated = useRef(new Animated.Value(0)).current;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const startAnim = () => {
+    setIsLoading(true);
+    Animated.loop(
+      Animated.timing(animated, {
+        toValue: 90,
+        duration: 4000,
+        useNativeDriver: false,
+      }),
+    ).start();
+  };
+
+  useEffect(() => {
+    let time: number | null = null;
+    time = isLoading
+      ? setTimeout(() => {
+          setIsLoading(false);
+        }, 1000)
+      : null;
+    return () => {
+      time && clearTimeout(time);
+    };
+  }, [isLoading]);
+
   return (
-    <TouchableWithoutFeedback onPress={onPress}>
+    <TouchableWithoutFeedback onPress={onPress} onPressIn={startAnim}>
       <View style={[styles.wrapper, style]}>
         <LinearGradient
           colors={['#CA1D7E', '#E35157', '#F2703F']}
@@ -40,6 +67,13 @@ const AvatarButton: React.FC<IAvatarButtonProps> = ({
           <View style={styles.container}>
             <Avatar {...rest} />
           </View>
+          <Animated.View
+            style={[
+              styles.loader,
+              isLoading && styles.visible,
+              {transform: [{rotate: animated}]},
+            ]}
+          />
         </LinearGradient>
         {username && <Text>{username}</Text>}
       </View>
@@ -53,17 +87,36 @@ const styles = StyleSheet.create({
   wrapper: {
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
   },
   linearContainer: {
     alignSelf: 'center',
     borderRadius: 200,
     padding: 2,
     overflow: 'hidden',
+    position: 'relative',
+    zIndex: 3,
   },
+  loader: {
+    opacity: 0,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderRadius: 200,
+    borderStyle: 'dashed',
+    borderColor: 'white',
+    borderWidth: 2,
+  },
+  visible: {opacity: 1},
   container: {
+    position: 'relative',
     padding: 2,
     borderRadius: 200,
     backgroundColor: Colors.WHITE,
     alignSelf: 'flex-start',
+    zIndex: 3,
   },
 });

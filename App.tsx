c0fad3d77b08edc5bf07df0fb7f086d136457a95
Artem Gorbach <gorbacartem90@gmail.com>
2020-07-10 11:31:16 +0300

    add splash screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 // react
-import React from 'react';
+import React, { useEffect } from 'react';
 // navigation
 import RootNavigator from './app/navigation/RootNavigator';
+import RNBootSplash from 'react-native-bootsplash';
 
-declare const global: {HermesInternal: null | {}};
+declare const global: { HermesInternal: null | {} };
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      RNBootSplash.hide({ duration: 250 });
+    }, 3000);
+  }, []);
+
   return <RootNavigator />;
 };
 
